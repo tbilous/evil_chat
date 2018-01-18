aaeafce3b18b87d05477f51b624f0aedb992ad0a
Taras Bilous <tbilous@yahoo.com>
2018-01-18 21:57:17 +0000

    part2 over

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
 
-  root to: "pages#home"
+  root to: "chat#show"
+
+  get  "/login", to: "auth#new"
+  post "/login", to: "auth#create"
 end
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,5 +1,7 @@
 import "init";
 import "components/page/page";
+import "components/auth-form/auth-form";
+import "components/chat/chat";
 import "./application.css";
 
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
+// document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
