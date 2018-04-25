ab0c7a8837b0c5407f95f8cc220ead6672383b29
Taras Bilous <tbilous@yahoo.com>
2018-04-25 10:14:19 +0300

    dev on localhost

--- lib/generators/component_generator.rb
+++ lib/generators/component_generator.rb
@@ -1,5 +1,5 @@
 class ComponentGenerator < Rails::Generators::Base
-  argument :component_name, required: true, desc: "Component name, e.g: button"
+  argument :component_name, required: true, desc: 'Component name, e.g: button'
 
   def create_view_file
     create_file "#{component_path}/_#{component_name}.html.erb"
